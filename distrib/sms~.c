db2ab1fb1ddee2a55b4d0fc404297339896fbb2f
Chloe Dequeker <ch.dequeker@gmail.com>
2014-12-26 22:31:57 +0100

    early sms

--- distrib/sms~.c
+++ distrib/sms~.c
@@ -1,39 +1,56 @@
 #include "sms~.h"
+#include <stdlib.h>
 #define BUFFER_LEN 4096
 
-_int *sms_tilde_perform (t_int *w){
+t_int *sms_tilde_perform (t_int *w){
+    t_sms_tilde* x = (t_sms_tilde*) w[1];
+    t_inlet* in_srcHarm = (t_inlet*) w[2];
+    t_inlet* in_srcMod = (t_inlet*) w[3];
+    t_inlet* in_synth = (t_inlet*) w[4];
+    t_inlet* in_msg = (t_inlet*) w[5];
+    t_outlet* out = (t_outlet*) w[6];
+    int vecSize = (int) w[7];
+
+    if (in_msg[1]->info == 1){ // if bypass
+        
+    }
+
+    
+
+    printf("coucou\n");
+    return 0;
+}
+
+void sms_tilde_dsp(t_sms_tilde *x,t_signal **sp){
+    dsp_add(sms_tilde_perform, 7, x, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, sp[4]->s_vec, sp[0]->s_n); 
 
 }
 
-void sms_tilde_dsp(t_duck_tilde *x,t_signal **sp){
-
-}
-
-void sms_tilde_free(t_duck_tilde *x){
+void sms_tilde_free(t_sms_tilde *x){
 	inlet_free(x->in1);
 	inlet_free(x->in2);
 	inlet_free(x->in3);
 	inlet_free(x->in4);
-	outlet_free(x->x_out);
+	outlet_free(x->out);
 	free(x->buffer);
 }
 
 void *sms_tilde_new(t_symbol *s, int argc, t_atom *argv){
 	t_sms_tilde *m;
 	m = (t_sms_tilde*)pd_new(sms_tilde_class);
-	m->x_in1 = inlet_new(&m->x_obj,&m->x_obj.ob_pd,&s_signal,&s_signal);
-	m->x_in2 = inlet_new(&m->x_obj,&m->x_obj.ob_pd,&s_signal,&s_signal);
- 	m->x_in3 = floatinlet_new(&m->x_obj,&m->seuil);
-  	m->x_in4 = pointerinlet_new(&m->x_obj,&m->info);
- 	m->x_out = outlet_new(&m->x_obj,&s_signal);
+	m->in1 = inlet_new(&m->x_obj,&m->x_obj.ob_pd,&s_signal,&s_signal);
+	m->in2 = inlet_new(&m->x_obj,&m->x_obj.ob_pd,&s_signal,&s_signal);
+ 	m->in3 = floatinlet_new(&m->x_obj,&m->seuil);
+ 	m->in4 = floatinlet_new(&m->x_obj,&m->info);
+ 	m->out = outlet_new(&m->x_obj,&s_signal);
 
- 	if(argc == 1){
- 		m->buffer = (t_sample*)malloc(atom_getint(argv) * sizeof (t_sample));
- 	}else{
+ 	if(argc < 2){
  		m->buffer = (t_sample*)malloc(BUFFER_LEN * sizeof (t_sample));
+ 	}else{
+ 		m->buffer = (t_sample*)malloc(atom_getint(argv) * sizeof (t_sample));
  	}
 
- 	return (void*m);
+ 	return (void*) m;
 }
 
 void sms_tilde_setup(void){
--- distrib/sms~.h
+++ distrib/sms~.h
@@ -1,28 +1,35 @@
+#ifndef SMS_DEF
+#define SMS_DEF
+
 #include "m_pd.h"
 
 static t_class *sms_tilde_class;
 
 typedef struct _sms_tilde
 {
-	 t_object x_obj;
-	 t_inlet *x_in1;
-	 t_inlet *x_in2;
-	 t_inlet *x_in3;
-	 t_inlet *x_in4;
-	 int buffer_size;
-	 t_float seuil;
-	 t_gpointer info;
-	 t_outlet *x_out;
-	 t_sample *buffer;
+	 t_object   x_obj;
+	 t_inlet    *in1;
+	 t_inlet    *in2;
+	 t_inlet    *in3;
+	 t_inlet    *in4;
+	 int        buffer_size;
+	 t_float    seuil;
+	 t_float    info;
+	 t_outlet   *out;
+	 t_sample   *buffer;
+	 t_sample   f;
 }t_sms_tilde;
+
 
 
 t_int *sms_tilde_perform (t_int *w);
 
-void sms_tilde_dsp(t_duck_tilde *x,t_signal **sp);
+void sms_tilde_dsp(t_sms_tilde *x,t_signal **sp);
 
-void sms_tilde_free(t_duck_tilde *x);
+void sms_tilde_free(t_sms_tilde *x);
 
-void *sms_tilde_new(void);
+void *sms_tilde_new(t_symbol *s, int argc, t_atom *argv);
 
 void sms_tilde_setup(void);
+
+#endif
